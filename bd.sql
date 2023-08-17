c8f8c05c73e1923fb09d17b1babb7e9e84295fa3
lucas-daniel202 <lucas.dan.melo@gmail.com>
2023-08-17 12:32:45 -0300

    alteração no banco de dados

--- bd.sql
+++ bd.sql
@@ -1,128 +1,156 @@
--- criação de BD para o cenário de E-commerce
--- drop database ecommerce;
-create database ecommerce;
-use ecommerce;
+-- Criação do banco de dados
+create database biblioteca;
+use biblioteca;
 
--- criar tabela cliente
-create table clients(
-		idClient int auto_increment primary key,
-        Fname varchar(10),
-        Minit char(3),
-        Lname varchar(20),
-        CPF char(11) not null,
-        Address varchar(30),
-        constraint unique_cpf_cliente unique(CPF)
-);
-alter table clients auto_increment = 1;
-
--- criar tabela produto
-create table product(
-		idProduct int auto_increment primary key,
-        Pname varchar(10) not null,
-        classification_kids bool default false,
-        category enum('Eletrônico', 'Vestimenta', 'Brinquedos', 'Alimentos', 'Móveis') not null,
-        avaliação float default 0,
-        size varchar(10)
+-- Tabela para autores
+create table autores (
+    idAutor int auto_increment primary key,
+    nomeAutor varchar(100) not null
 );
 
--- criar tabela pedido
-create table orders(
-		idOrder int auto_increment primary key,
-        idOrderClient int,
-        orderStatus enum('Cancelado', 'Confirmado', 'Em processamento') default 'Em processamento',
-        orderDescription varchar(255),
-        sendValue float default 10,
-        paymentCash bool default false,
-        constraint fk_orders_client foreign key (idOrderClient) references clients(idClient)
-        on update cascade
+-- Tabela para editoras
+create table editoras (
+    idEditora int auto_increment primary key,
+    nomeEditora varchar(100) not null,
+    localizacao varchar(100)
 );
 
--- criar tabela estoque
-create table productStorage(
-		idProdStorage int auto_increment primary key,
-        storageLocation varchar(255),
-        quantify int default 0
+-- Tabela para gêneros dos livros
+create table generos (
+    idGenero int auto_increment primary key,
+    nomeGenero varchar(50) not null
 );
 
--- criar tabela fornecedor
-create table supplier(
-		idSupplier int auto_increment primary key,
-        SocialName varchar(255) not null,
-        CNPJ char(15) not null,
-        contact char(11) not null,
-        constraint unique_supplier unique (CNPJ)
-);
-        
--- criar tabela vendedor
-create table seller(
-		idSeller int auto_increment primary key,
-        SocialName varchar(255) not null,
-        AbstName varchar(255),
-        CNPJ char(15),
-        CPF char(9),
-        location varchar(255),
-        contact char(11) not null,
-        constraint unique_cnpj_seller unique (CNPJ),
-        constraint unique_cpf_seller unique (CPF)
+-- Tabela para livros
+create table livros (
+    idLivro int auto_increment primary key,
+    titulo varchar(255) not null,
+    idAutor int,
+    idEditora int,
+    idGenero int,
+    anoPublicacao int,
+    foreign key (idAutor) references autores(idAutor),
+    foreign key (idEditora) references editoras(idEditora),
+    foreign key (idGenero) references generos(idGenero)
 );
 
-create table productSeller(
-		idPseller int,
-        idPproduct int,
-        prodQuantity int default 1,
-        primary key (idPseller, idPproduct),
-        constraint fk_product_seller foreign key (idPseller) references seller(idSeller),
-        constraint fk_product_product foreign key (idPproduct) references product(idProduct)
+-- Tabela para leitores
+create table leitores (
+    idLeitor int auto_increment primary key,
+    nomeLeitor varchar(100) not null,
+    dataNascimento date,
+    email varchar(100)
 );
 
-create table productOrder(
-		idPOproduct int,
-        idPOorder int,
-        poQuantity int default 1,
-        poStatus enum('Disponível', 'Sem estoque') default 'Disponível',
-        primary key (idPOproduct, idPOorder),
-        constraint fk_productorder_seller foreign key (idPOproduct) references product(idProduct),
-        constraint fk_productorder_product foreign key (idPOorder) references orders(idOrder)
+-- Tabela para empréstimos
+create table emprestimos (
+    idEmprestimo int auto_increment primary key,
+    idLivro int,
+    idLeitor int,
+    dataEmprestimo date,
+    dataDevolucao date,
+    foreign key (idLivro) references livros(idLivro),
+    foreign key (idLeitor) references leitores(idLeitor)
 );
 
-create table storageLocation(
-		idLproduct int,
-        idLstorage int,
-        location varchar(255) not null,
-        primary key (idLproduct, idLstorage),
-        constraint fk_storage_location_product foreign key (idLproduct) references product(idProduct),
-        constraint fk_storage_location_storage foreign key (idLstorage) references productStorage(idProdStorage)
-);
+-- Inserção de autores
+insert into autores (nomeAutor) values
+    ('Machado de Assis'),
+    ('Clarice Lispector'),
+    ('José Saramago'),
+    ('Gabriel Garcia Marquez'),
+    ('J.K. Rowling'),
+    ('George Orwell'),
+    ('Agatha Christie'),
+    ('Haruki Murakami'),
+    ('Jane Austen'),
+    ('Stephen King');
 
-create table productSupplier(
-		idPsSupplier int,
-        idPsProduct int,
-        quantity int not null,
-        primary key (idPsSupplier, idPsProduct),
-        constraint fk_product_supplier_supplier foreign key (idPsSupplier) references supplier(idSupplier),
-        constraint fk_product_supplier_product foreign key (idPsProduct) references product(idProduct)
-);
+-- Inserção de editoras
+insert into editoras (nomeEditora, localizacao) values
+    ('Companhia das Letras', 'São Paulo'),
+    ('Editora Record', 'Rio de Janeiro'),
+    ('Editora Leya', 'Lisboa'),
+    ('Penguin Random House', 'Nova York'),
+    ('Bloomsbury', 'Londres'),
+    ('Houghton Mifflin Harcourt', 'Boston'),
+    ('HarperCollins', 'Londres'),
+    ('Kodansha', 'Tóquio'),
+    ('Vintage', 'Nova York'),
+    ('Scribner', 'Nova York');
 
--- Inserir clientes
-insert into clients(Fname, Minit, Lname, CPF, Address) values
-    ('John', 'A', 'Doe', '12345678901', '123 Main St'),
-    ('Jane', 'B', 'Smith', '98765432109', '456 Elm St');
+-- Inserção de gêneros de livros
+insert into generos (nomeGenero) values
+    ('Ficção'),
+    ('Mistério'),
+    ('Romance'),
+    ('Fantasia'),
+    ('Distopia'),
+    ('Suspense'),
+    ('Aventura'),
+    ('História'),
+    ('Sci-Fi'),
+    ('Clássico');
 
--- Inserir produtos
-insert into product(Pname, classification_kids, category, avaliação, size) values
-    ('Smartphone', false, 'Eletrônico', 4.5, 'Medium'),
-    ('Camiseta', true, 'Vestimenta', 3.8, 'Large');
+-- Inserção de livros
+insert into livros (titulo, idAutor, idEditora, idGenero, anoPublicacao) values
+    ('Dom Casmurro', 1, 1, 3, 1899),
+    ('A Hora da Estrela', 2, 2, 3, 1977),
+    ('Ensaio sobre a Cegueira', 3, 3, 1, 1995),
+    ('Cem Anos de Solidão', 4, 4, 3, 1967),
+    ('Harry Potter e a Pedra Filosofal', 5, 5, 4, 1997),
+    ('1984', 6, 6, 5, 1949),
+    ('Assassinato no Expresso do Oriente', 7, 7, 2, 1934),
+    ('Norwegian Wood', 8, 8, 6, 1987),
+    ('Orgulho e Preconceito', 9, 9, 3, 1813),
+    ('O Iluminado', 10, 10, 2, 1977);
 
--- Inserir fornecedores
-insert into supplier(SocialName, CNPJ, contact) values
-    ('TechSuppliers', '12345678901234', '98765432101'),
-    ('FashionWholesalers', '98765432102345', '12345678901');
+-- Inserção de leitores
+insert into leitores (nomeLeitor, dataNascimento, email) values
+    ('Ana Silva', '1990-05-15', 'ana@email.com'),
+    ('João Santos', '1985-09-20', 'joao@email.com'),
+    ('Maria Oliveira', '1998-02-10', 'maria@email.com'),
+    ('Pedro Almeida', '1982-11-30', 'pedro@email.com'),
+    ('Carla Rodrigues', '2000-07-25', 'carla@email.com'),
+    ('Ricardo Pereira', '1976-04-18', 'ricardo@email.com'),
+    ('Camila Souza', '1995-08-05', 'camila@email.com'),
+    ('Fernando Lima', '1989-01-22', 'fernando@email.com'),
+    ('Isabela Gonçalves', '1993-12-02', 'isabela@email.com'),
+    ('Lucas Marques', '1997-06-12', 'lucas@email.com');
 
--- Inserir vendedores
-insert into seller(SocialName, AbstName, CNPJ, CPF, location, contact) values
-    ('ElectronicsRUs', 'E. R. Us', '12345678901', '987654321', '456 Tech St', '98765432101');
+-- Inserção de empréstimos
+insert into emprestimos (idLivro, idLeitor, dataEmprestimo, dataDevolucao) values
+    (1, 1, '2023-08-01', '2023-08-15'),
+    (2, 2, '2023-08-03', '2023-08-17'),
+    (3, 3, '2023-08-05', '2023-08-20'),
+    (4, 4, '2023-08-07', '2023-08-22'),
+    (5, 5, '2023-08-09', '2023-08-24'),
+    (6, 6, '2023-08-11', '2023-08-26'),
+    (7, 7, '2023-08-13', '2023-08-28'),
+    (8, 8, '2023-08-15', '2023-08-30'),
+    (9, 9, '2023-08-17', '2023-09-03'),
+    (10, 10, '2023-08-19', '2023-09-05');
+    
+-- consultas
+select livros.titulo, autores.nomeAutor, editoras.nomeEditora
+from livros
+inner join autores on livros.idAutor = autores.idAutor
+inner join editoras on livros.idEditora = editoras.idEditora;
 
--- Inserir pedidos
-insert into orders(idOrderClient, orderStatus, orderDescription, sendValue, paymentCash) values
-    (1, 'Em processamento', 'Pedido de smartphone', 10, false),
-    (2, 'Confirmado', 'Pedido de camiseta', 5, true);
+select titulo, anoPublicacao
+from livros
+where anoPublicacao > 1950;
+
+select count(*) as total_emprestados
+from emprestimos;
+
+select titulo, anoPublicacao
+from livros
+order by anoPublicacao;
+
+select autores.nomeAutor, count(livros.idLivro) as total_livros
+from autores
+inner join livros on autores.idAutor = livros.idAutor
+group by autores.nomeAutor
+order by total_livros desc
+limit 3;
